ecc3a2c30415f96d2ae2439b5195a09cabf99002
shanizil <shanizil100@walla.com>
2018-08-24 17:29:40 +0300

    24/8 6

--- logData.js
+++ logData.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var schema = mongoose.Schema;
 
 var logSchema = new schema({
-	_id: {type: String, index:1, required:true, unique:true},
+	_id: {type: String},
 	logData: String,
 	logDate: String
 }, {collection: 'logs'});
