4db2c21ba1867d91219d06365a83ee12e794517e
shanizil <shanizil100@walla.com>
2018-08-26 12:46:47 +0300

    upload 8

--- ToBscServer.js
+++ ToBscServer.js
@@ -27,15 +27,20 @@ app.use(cors({origin: '*'}));
 
 app.set('port',port);
 app.use('/', express.static('./public'));
-app.use(
-    (req,res,next) => {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers",
-            "Origin, X-Requested-With, Content-Type, Accept");
-        next();
-    });
+// app.use(
+//     (req,res,next) => {
+//         res.header("Access-Control-Allow-Origin", "*");
+//         res.header("Access-Control-Allow-Headers",
+//             "Origin, X-Requested-With, Content-Type, Accept");
+//         next();
+//     });
 
-
+app.use(function(req, res, next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.set("Content-Type", "application/json");
+    next();
+});
 
 app.get('/', (req, res) => {
    res.sendFile(`${__dirname}/index.html`);
