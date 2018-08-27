6e6d82d2a639dff2e8247422c702a76bb9718eef
shanizil <shanizil100@walla.com>
2018-08-27 17:59:58 +0300

    upload

--- logsController.js
+++ logsController.js
@@ -1,19 +1,48 @@
 const   mongoose = require('mongoose'),
         parser = require('json-parser'),
+        logs = require('./logsData'),
         http = require('http');
+//var log = require('./logData');
+
+
         options = {
             server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } },
             replset: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } }
         };
-var log = require('./logData');
-var logs = require('./logsData');
 
+
+// exports.getAllLogs = function(req, res){
+//         Logs.findOne({}).where('logsType').equals(req.body.logsType).
+//         exec (function(err, data){
+//             if(err) {
+//                 console.log(err);
+//                 return res.status(500).send();
+//             }
+//             // console.log(JSON.stringify(data.logsArr, null, 2));
+//             console.log("Get Logs Successfully");
+//             return res.status(200).send(data.logsArr);
+//         })
+
+//          return res.status(404).send();
+// };
 
 module.exports={
 
-        getAllLogs(){
-            return logs.find();
-        }
+
+        getAllLogs(req,res){
+        logs.findOne({logsType : req.body.logsType}, (err,result)=>{
+            if(err || !result){
+                return res.status(500).json(`{logs not exists:${err}}`);
+            }
+            else {
+                console.log("Get Logs Successfully");
+                return res.status(200).send(result.logsArr);
+            }
+        });
+     }
+    //     getAllLogs(){
+    //         return Logs.findOne();
+    //     }
 
        // getAllUserLogin(){
        //  return logs.find();
